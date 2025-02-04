f607e38aa74bc945b4f51ba33def64b9e542fe3f
Andrew Cortis <andrew.cortis@mcast.edu.mt>
2025-02-04 10:02:46 +0100

    Added example

---
+++ Ref_Value_Type_Example/DataClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ref_Value_Type_Example
+{
+    public class DataClass
+    {
+        public int DataValue { get; set; } = 0;
+
+        public DataClass() { }
+
+        public DataClass(int dataValue)
+        {
+            DataValue = dataValue;
+        }
+    }
+}
--- Ref_Value_Type_Example/Program.cs
+++ Ref_Value_Type_Example/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Ref_Value_Type_Example;
+
+Console.WriteLine("Value Type Variables:");
+
+int a = 5;
+int b = 6;
+
+Console.WriteLine($"The value of a is {a}");    // 5
+Console.WriteLine($"The value of b is {b}");    // 6
+
+a = b;
+Console.WriteLine("a=b");
+Console.WriteLine($"The value of a is {a}");    // 6
+Console.WriteLine($"The value of b is {b}");    // 6
+
+b = 10;
+
+Console.WriteLine("b=10");
+Console.WriteLine($"The value of a is {a}");    // 6
+Console.WriteLine($"The value of b is {b}");    // 10
+
+Console.WriteLine();
+Console.WriteLine("Reference Type Variables:");
+
+DataClass c = new DataClass(5);
+DataClass d = new DataClass(6);
+
+Console.WriteLine($"The value of c is {c.DataValue}");    // 5
+Console.WriteLine($"The value of d is {d.DataValue}");    // 6
+
+c = d;
+Console.WriteLine("c=d");
+Console.WriteLine($"The value of c is {c.DataValue}");    // 6
+Console.WriteLine($"The value of d is {d.DataValue}");    // 6
+
+d.DataValue = 10;
+Console.WriteLine("d.DataValue = 10");
+Console.WriteLine($"The value of c is {c.DataValue}");    // 10
+Console.WriteLine($"The value of d is {d.DataValue}");    // 10
