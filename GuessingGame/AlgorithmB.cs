6a89f9d3cb622a275bb64a6e8fafd86b7a8d9062
Andrew Cortis <andrew.cortis@mcast.edu.mt>
2025-02-11 09:49:53 +0100

    Added Algorithm B and some tests.

---
+++ GuessingGame/AlgorithmB.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    internal class AlgorithmB
+    {
+        private SecretNumber secretNumber;
+        internal AlgorithmB(SecretNumber secretNumber)
+        {
+            this.secretNumber = secretNumber;
+        }
+
+        /// <summary>
+        /// Guess all the numbers one after the other, starting from 1 up to the maximum number!
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        internal (int, int) Guess()
+        {
+            int minNumber = 1;
+            int maxNumber = secretNumber.MaxNumber;
+
+            int numberToGuess = maxNumber / 2;
+
+            while (true)
+            {
+                int comparisonResult = secretNumber.Guess(numberToGuess);
+
+                if (comparisonResult == 0)
+                    return (numberToGuess, secretNumber.TotalGuesses);
+
+                // otherwise, we have not guessed!
+                // try again
+                if (comparisonResult < 0)
+                {
+                    minNumber = numberToGuess + 1;
+                }
+                else if (comparisonResult > 0)
+                {
+                    maxNumber = numberToGuess - 1;   
+                }
+
+                numberToGuess = (minNumber + maxNumber) / 2;
+            }
+            
+        }
+    }
+}
--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -1,7 +1,47 @@
 ﻿using GuessingGame;
+using System.Diagnostics;
 
-SecretNumber s = new SecretNumber(100);
-AlgorithmA a = new AlgorithmA(s);
-(int, int) result = a.Guess();
+//List<int> times = TestAlgorithmA();
 
-Console.WriteLine($"Algorithm A has completed successfully! The secret number is {result.Item1} and it took {result.Item2} guesses to find!");
+int problemSize = 1024*1024*1024;
+int runs = problemSize;
+
+/*
+SecretNumber s = new SecretNumber(problemSize);
+AlgorithmB b = new AlgorithmB(s);
+(int, int) result = b.Guess();
+Console.WriteLine($"Algorithm B has completed successfully! The secret number is {result.Item1} and it took {result.Item2} guesses to find!");
+*/
+
+List<int> times = new List<int>();
+
+for (int i = 0; i < runs; i++)
+{
+    SecretNumber s = new SecretNumber(problemSize, i + 1);
+    AlgorithmB a = new AlgorithmB(s);
+    (int, int) result = a.Guess();
+    times.Add(result.Item2);
+}
+
+Console.WriteLine($"The mean time to find the secret number is {times.Average()}");
+
+static List<int> TestAlgorithmA()
+{
+    // problem size (the maximum secret number)
+    int problemSize = 1000;
+    int runs = problemSize;
+
+    List<int> times = new List<int>();
+
+    for (int i = 0; i < runs; i++)
+    {
+        SecretNumber s = new SecretNumber(problemSize, i + 1);
+        AlgorithmA a = new AlgorithmA(s);
+        (int, int) result = a.Guess();
+        times.Add(result.Item2);
+    }
+
+    return times;
+}
+
+//Console.WriteLine($"Algorithm A has completed successfully! The secret number is {result.Item1} and it took {result.Item2} guesses to find!");
