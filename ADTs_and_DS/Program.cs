1cdc566d1549a2acd3f215dcbc4944606e164703
Andrew Cortis <andrew.cortis@mcast.edu.mt>
2025-02-21 09:53:27 +0100

    Implemented most of the ABV.

--- ADTs_and_DS/Program.cs
+++ ADTs_and_DS/Program.cs
@@ -5,3 +5,41 @@ using ADTs_and_DS.VectorImplementations;
 ArrayBasedVector<string> arrayBasedVector = new ArrayBasedVector<string>( new []{ "Dave", "Bernice", "Eve", "Charles", "Adam" });
 
 Console.WriteLine(arrayBasedVector);
+
+#region GetElementAtRank examples
+int rank = 2;
+Console.WriteLine($"The element at rank {rank} is {arrayBasedVector.GetElementAtRank(rank)}");
+
+rank = 0;
+Console.WriteLine($"The element at rank {rank} is {arrayBasedVector.GetElementAtRank(rank)}");
+
+rank = 4;
+Console.WriteLine($"The element at rank {rank} is {arrayBasedVector.GetElementAtRank(rank)}");
+
+try
+{
+    rank = -1;
+    Console.WriteLine($"The element at rank {rank} is {arrayBasedVector.GetElementAtRank(rank)}");
+}
+catch (ArgumentOutOfRangeException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+#endregion
+
+#region InsertElementAtRankExamples
+arrayBasedVector.InsertElementAtRank(2, "Mavis");
+
+Console.WriteLine(arrayBasedVector);
+
+arrayBasedVector.InsertElementAtRank(0, "Fred");
+
+Console.WriteLine(arrayBasedVector);
+
+arrayBasedVector.InsertElementAtRank(7, "George");
+Console.WriteLine(arrayBasedVector);
+#endregion
+
+Console.WriteLine($"Removing element at rank 1. Element removed is {arrayBasedVector.RemoveElementAtRank(1)}");
+Console.WriteLine(arrayBasedVector);
+
--- ADTs_and_DS/VectorImplementations/ArrayBasedVector.cs
+++ ADTs_and_DS/VectorImplementations/ArrayBasedVector.cs
@@ -31,7 +31,7 @@ namespace ADTs_and_DS.VectorImplementations
             }
         }
 
-        private const int INITIAL_LENGTH = 8;
+        private const int INITIAL_LENGTH = 5;
 
         /// <summary>
         /// The array that will store our elements (of type T)
@@ -62,27 +62,129 @@ namespace ADTs_and_DS.VectorImplementations
 
         public T GetElementAtRank(int rank)
         {
-            throw new NotImplementedException();
-        }
+            // Validation
+            if (rank < 0 || rank >= count)
+            {
+                // this element at the rank being passed as a parameter does not exist!
+                // Remember: the rank of the 1st element is 0, the rank of the last element is count - 1
+                throw new ArgumentOutOfRangeException(nameof(rank), $"The acceptable values for rank are between 0 and {count - 1}.");
+            }
 
-        public void InsertElementAtRank(int rank, T element)
-        {
-            throw new NotImplementedException();
+            // The element at the rank passed as a parameter is at index rank
+            return array[rank];
         }
-
         public bool IsEmpty()
         {
-            throw new NotImplementedException();
+            // the ABV is empty when the number of elements is 0
+            /*
+            if (count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            */
+
+            // Equivalent to the code above.
+            return count == 0;
         }
+
+        /// <summary>
+        /// The operation takes an element and places the element in the position indicated by the rank
+        /// Since there may be other elements (at the rank and behind the given rank), we need to make space for the new element
+        /// </summary>
+        /// <param name="rank"></param>
+        /// <param name="element"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void InsertElementAtRank(int rank, T element)
+        {
+            // Validation
+            if (rank < 0 || rank > count)
+            {
+                // Possible positions are between rank 0 (first element) and count (this mean we will append the new element)
+                throw new ArgumentOutOfRangeException(nameof(rank), $"The acceptable values for rank are between 0 and {count}.");
+            }
+
+            // Check whether the array is full
+            if (array.Length == count)
+            {
+                // the array is full
+
+                // Create a new array which is twice the size of the original array
+                T[] newArray = new T[array.Length * 2];
+
+                // Copy everything over
+                array.CopyTo( newArray, 0 );
+
+                /* Easier to read the above call, in reality, we are copying all the elements over to the new array
+                // Equivalent to the CopyTo method...
+                for (int i = 0; i < array.Length; i++)
+                {
+                    newArray[i] = array[i];
+                }
+                */
+
+                // replace the array with the new, larger array
+                array = newArray;
+            }
+
+            // There is now available space...
+
+            // Add some code here to shift the elements
+
+            // This code causes a problem! We would overwrite our data...
+            /* Bug in this code!
+            for (int i = rank; i < count; i++)
+            {
+                array[i + 1] = array[i];
+            }
+            */
+
+            // start shifting from the last element (at count - 1),
+            // keep shifting until the position at rank is freed
+            for (int i = count - 1; i >= rank; i--)
+            {
+                array[i + 1] = array[i];
+            }
+
+            // place the new element in the correct position
+            array[rank] = element;
+            // there is now 1 new element, so increment count
+            count++;
+        }
+
 
         public T RemoveElementAtRank(int rank)
         {
-            throw new NotImplementedException();
+            T elementToBeRemoved = GetElementAtRank(rank);
+
+            for (int i = rank; i < count - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+
+            // remove the value stored where the last element was before
+            array[count - 1] = default!;
+
+            count--;
+            return elementToBeRemoved;
         }
 
+        /// <summary>
+        /// 1. Check whether the rank passed as a parameter is valid!
+        /// 2. Get the element you find at rank
+        /// 3. Copy the new element to the rank
+        /// 4. Return the old element
+        /// </summary>
+        /// <param name="rank">The rank of the element to be replaced</param>
+        /// <param name="newElement">The new element to replace the old element at the given rank</param>
+        /// <returns>The old element that was found at the rank</returns>
+        /// <exception cref="NotImplementedException">Exercise for you!</exception>
         public T ReplaceElementAtRank(int rank, T newElement)
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException("Exercise! To do by 24th February (or next lecture)");
         }
 
         public override string ToString()
