0a4079fab15dbf09d28cf0ba24cffe00ba9d899d
Andrew Cortis <andrew.cortis@mcast.edu.mt>
2025-03-07 10:28:40 +0100

    Created structure for SLL

--- ADTs_and_DS/Linked_Lists/SinglyLinkedList.cs
+++ ADTs_and_DS/Linked_Lists/SinglyLinkedList.cs
@@ -13,5 +13,57 @@ namespace ADTs_and_DS.Linked_Lists
         // private SinglyLinkedListNode<T>? tail;
 
         private int count = 0;
+
+        public int Count { get { return count; } }
+
+        public bool IsEmpty { get { return count == 0; } }
+
+        public SinglyLinkedListNode<T>? Head { get; private set; }
+
+        public SinglyLinkedListNode<T>? Tail { get; private set; }
+
+        /// <summary>
+        /// Insert a new element and place it at the head of list
+        /// </summary>
+        /// <param name="element"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void InsertFirst(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Insert a new element at the tail of list
+        /// </summary>
+        /// <param name="element"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void InsertLast(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Remove the element at the head of list
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public T RemoveFirst() { throw new NotImplementedException(); }
+
+        /// <summary>
+        /// Remove the element at the tail of list
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public T RemoveLast() {  throw new NotImplementedException(); }
+
+        public void InsertAfter(SinglyLinkedListNode<T> cursor, T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool InsertBefore(SinglyLinkedListNode<T> cursor, T eleent)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
