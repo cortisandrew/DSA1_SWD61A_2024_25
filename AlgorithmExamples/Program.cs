ce219f796c196dd3e96958c951cf617074eb1495
Andrew Cortis <andrew.cortis@mcast.edu.mt>
2025-03-25 10:00:20 +0100

    Added some algorithms

---
+++ AlgorithmExamples/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+int[] problemSizes = new int[] { 2, 4, 8, 16, 32, 64, 128 };
+
+
+foreach (int n in problemSizes) {
+    int time = G(n);
+
+    Console.WriteLine($"{n}, {time}");
+}
+*/
+
+int n = 4;
+Console.WriteLine($"{n}, {H(n)}");
+
+
+int G(int n)
+{
+    int i = n;
+    int sum = 0;
+
+    while (i > 1)
+    {
+        i = i / 2;
+        sum++;
+    }
+
+    return sum;
+}
+
+int H(int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine();
+        Console.Write($"i: {i}, j: ");
+
+        for (int j = i; j < n; j++)
+        {
+            Console.Write($"{j} ");
+            sum++;
+        }
+    }
+    Console.WriteLine();
+    return sum;
+}
